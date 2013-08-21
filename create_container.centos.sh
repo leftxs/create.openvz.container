9f65cf88ac475b9448bc507626e57cb289ad4720
sven <sven@so36.net>
2013-08-21 11:42:44 +0200

    Update create centos script

--- create_container.centos.sh
+++ create_container.centos.sh
@@ -26,40 +26,40 @@ else
         NEWID=$(($CURID + 1))                   || error_exit "Could not find next available ID"
 fi
 
-echo
-echo "Available templates:"
-ls $VZDIR/template/cache/ | sed "s/\.tar\.gz$//"
+#echo
+#echo "Available templates:"
+#ls $VZDIR/template/cache/ | sed "s/\.tar\.gz$//"
 
-echo
-read -p "Enter template name:    " VZTMPLT
-[ -f "$VZDIR/template/cache/$VZTMPLT.tar.gz" ]  || error_exit "Template does not exist"
-read -p "Enter hostname (fqdn):     " VZHOST
-read -p "Enter IP address:          " VZIP
-read -p "Ram: like 512M or 1G	    " VZRAM
-read -p "Swap: like 1G or 2G	    " VZSWAP
-read -p "Disk qouta: like 1G or 2G  " DISKQOUTA
-read -p "Disk boost: like 1G or 2G  " DISKBOOST
-read -p "Create container? [y/N]    " CREATE
+#echo
+#read -p "Enter template name:    " VZTMPLT
+#[ -f "$VZDIR/template/cache/$VZTMPLT.tar.gz" ]  || error_exit "Template does not exist"
+#read -p "Enter hostname (fqdn):     " VZHOST
+#read -p "Enter IP address:          " VZIP
+#read -p "Ram: like 512M or 1G	    " VZRAM
+#read -p "Swap: like 1G or 2G	    " VZSWAP
+#read -p "Disk qouta: like 1G or 2G  " DISKQOUTA
+#read -p "Disk boost: like 1G or 2G  " DISKBOOST
+#read -p "Create container? [y/N]    " CREATE
 
-case "$CREATE" in
-        y | yes | Y | YES | Yes )
+#case "$CREATE" in
+#        y | yes | Y | YES | Yes )
 
                 $VZCTL create "$NEWID" --ostemplate "$VZTMPLT" && \
-                $VZCTL set "$NEWID" --ipadd "$VZIP" --save && \
-                $VZCTL set "$NEWID" --hostname "$VZHOST" --save && \
+                $VZCTL set "$NEWID" --ipadd "centos-6-x86_64" --save && \
+                $VZCTL set "$NEWID" --hostname "centos6-test-installer" --save && \
                 $VZCTL set "$NEWID" --nameserver 8.8.8.8 --save && \
-                $VZCTL set "$NEWID" --ram "$VZRAM" --swap "$VZSWAP" --save && \
-	        $VZCTL set "$NEWID" --diskspace "$DISKQOUTA":"$DISKBOOST" --save && \
+                $VZCTL set "$NEWID" --ram "1G" --swap "2G" --save && \
+	        $VZCTL set "$NEWID" --diskspace "8G":"9G" --save && \
                 $VZCTL set "$NEWID" --onboot yes --save && \
                 echo
                 echo "Container $NEWID has been created. You can start it like this:"
                 echo "vzctl start $NEWID"
                 echo
-                ;;
+#                ;;
 
-        * )
-                echo "Not creating container $NEWID..."
-                echo
-                ;;
-esac
+#        * )
+#                echo "Not creating container $NEWID..."
+#                echo
+#                ;;
+#esac
 
